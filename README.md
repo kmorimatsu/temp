928968b1611a937e70c1c979da0986d5c5b1d255
Katsumi <kmorimatsu@users.sourceforge.jp>
2022-05-12 12:37:31 -0700

    FIFO test

--- README.md
+++ README.md
@@ -4,3 +4,29 @@ Temporary git archive
 ## about Temp
 
 Currently testing the multicore function of Raspberry Pi Pico
+
+## FIFO
+
+Each FIFO is 32 bits wide, and eight entries deep. One of the FIFOs can only be written by core 0, and read by core 1. The other can only be written by core 1, and read by core 0.
+
+## Result
+```console
+Hello, multicore world!
+Hello, core1: 2000!
+Hello, core0: 1000!
+Hello, core1: 2001!
+Hello, core0: 1001!
+Hello, core1: 2002!
+Hello, core0: 1002!
+Hello, core1: 2003!
+Hello, core0: 1003!
+Hello, core1: 2004!
+Hello, core0: 1004!
+Hello, core1: 2005!
+Hello, core0: 1005!
+Hello, core1: 2006!
+Hello, core0: 1006!
+Hello, core1: 2007!
+Hello, core0: 1007!
+Hello, core1: 2008!
+```
--- hello_usb.c
+++ hello_usb.c
@@ -9,24 +9,23 @@
 #include "pico/multicore.h"
 
 void core1_entry() {
-	printf("Hello, core1!\n");
-	// multicore_reset_core1(); // This function doesn't work by core1
+	static int i=1000;
+	multicore_fifo_push_blocking(i++);
+	printf("Hello, core1: %d!\n",multicore_fifo_pop_blocking());
 }
 
 int main() {
+	int i=2000;
 	stdio_init_all();
 	sleep_ms(3000);
 	printf("Hello, multicore world!\n");
-	multicore_reset_core1();
-	multicore_launch_core1(core1_entry);
-	sleep_ms(1000);
-	printf("Hello, core0!\n");
-	sleep_ms(1000);
-	multicore_reset_core1();// This line is needed to start core1 again. Use this function by core0
-	multicore_launch_core1(core1_entry);
 	while (true) {
 		sleep_ms(1000);
-		printf("Hello, world!\n");
+		multicore_reset_core1();
+		multicore_launch_core1(core1_entry);
+		multicore_fifo_push_blocking(i++); // This function must be called after calling multicore_launch_core1()
+		sleep_ms(1000);
+		printf("Hello, core0: %d!\n",multicore_fifo_pop_blocking());
 	}
 	return 0;
 }
