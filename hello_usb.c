3288baa0533668737dc639147671caf5cbb0cd25
Katsumi <kmorimatsu@users.sourceforge.jp>
2022-05-12 12:04:53 -0700

    Initial test code to use core1

---
+++ hello_usb.c
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+
+void core1_entry() {
+	printf("Hello, core1!\n");
+}
+
+int main() {
+	stdio_init_all();
+	sleep_ms(3000);
+	printf("Hello, multicore world!\n");
+	multicore_reset_core1();
+	multicore_launch_core1(core1_entry);
+	sleep_ms(1000);
+	printf("Hello, core0!\n");
+	sleep_ms(1000);
+	multicore_reset_core1();// This line is needed to start core1 again
+	multicore_launch_core1(core1_entry);
+	while (true) {
+		sleep_ms(1000);
+		printf("Hello, world!\n");
+	}
+	return 0;
+}
