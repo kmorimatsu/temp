d56c068e665caee13803b20f2c36c59f366ea364
Katsumi <kmorimatsu@users.sourceforge.jp>
2022-05-12 12:10:05 -0700

    multicore_reset_core1() is valid only by core0

--- hello_usb.c
+++ hello_usb.c
@@ -10,6 +10,7 @@
 
 void core1_entry() {
 	printf("Hello, core1!\n");
+	// multicore_reset_core1(); // This function doesn't work by core1
 }
 
 int main() {
@@ -21,7 +22,7 @@ int main() {
 	sleep_ms(1000);
 	printf("Hello, core0!\n");
 	sleep_ms(1000);
-	multicore_reset_core1();// This line is needed to start core1 again
+	multicore_reset_core1();// This line is needed to start core1 again. Use this function by core0
 	multicore_launch_core1(core1_entry);
 	while (true) {
 		sleep_ms(1000);
